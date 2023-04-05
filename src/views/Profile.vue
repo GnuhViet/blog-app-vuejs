edd299c11f4c0638f74ec754165d948c773087ab
GNUH <34486659+nvh202sua@users.noreply.github.com>
2023-04-05 20:05:22 +0700

    update profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,18 +1,42 @@
 <template>
   <body>
-    <div class="profile" v-for="row in table_data" :key="row.id" >
-      <img src="../assets/logo.png" alt="User Avatar">
-      <h1>{{ row.username }}</h1>
+    <div v-if="!isEditing" class="profile">
+      <img
+        :src= "table_data.avatar ? 'https://localhost:7185' + table_data.avatar : (imagePreview ? imagePreview : 'https://cdn-icons-png.flaticon.com/512/149/149071.png')"
+        alt="User Avatar">
+      <h1>{{ table_data.username }}</h1>
       <table>
         <tr>
+          <td>Full name:</td>
+          <td>{{ table_data.fullName }}</td>
+        </tr>
+        <tr>
           <td>Email:</td>
-          <td>{{ row.email }}</td>
+          <td>{{ table_data.email }}</td>
         </tr>
         <tr>
           <td>Phone:</td>
-          <td>(123) 456-7890</td>
+          <td>{{ table_data.phoneNumber }}</td>
         </tr>
       </table>
+      <button @click="isEditing = true">Edit</button>
+    </div>
+    <div v-else class="profile">
+      <img :src="table_data.avatar ? 'https://localhost:7185' + table_data.avatar : (imagePreview ? imagePreview : 'https://cdn-icons-png.flaticon.com/512/149/149071.png')" alt="User Avatar"
+        v-if="isEditing" onclick="document.getElementById('input-image').click()">
+      <form @submit.prevent="saveData">
+        <input id="input-image" type="file" style="display: none" accept="image/*" @change="handleImageUpload">
+        <label class="username-label">Username:</label>
+        <input class="username-input" type="text" v-model="table_data.username" readonly>
+        <label>Full name:</label>
+        <input type="text" v-model="table_data.fullName">
+        <label>Email:</label>
+        <input type="email" v-model="table_data.email">
+        <label>Phone:</label>
+        <input type="tel" v-model="table_data.phoneNumber">
+        <button type="submit">Save</button>
+        <button type="button" @click="cancelEdit">Cancel</button>
+      </form>
     </div>
   </body>
 </template>
@@ -20,14 +44,16 @@
 <script>
 import axios from 'axios';
 import { useStore } from 'vuex';
-import imageu from "../assets/logo.png"
 export default {
   components: {
-    
+
   },
   data() {
     return {
-      table_data: []
+      table_data: {},
+      isEditing: false,
+      imageFile: null,
+      imagePreview: null
     }
   },
   created() {
@@ -38,7 +64,7 @@ export default {
       var token = sessionStorage.getItem("JWT");
 
       const headers = {
-        'Authorization': "Bearer " +  token,
+        'Authorization': "Bearer " + token,
       }
 
       axios.get("https://localhost:7185/api/Account/UserDetails", {
@@ -47,13 +73,73 @@ export default {
         .then((res) => {
           console.log(res);
           let resc = res.data.user;
-          this.table_data = [resc]; 
+          this.blogUserId = resc.blogUserId;
+          this.table_data = resc;
           console.log(this.table_data);
         })
         .catch((error) => {
           console.log(error);
         });
-    }
+    },
+    handleImageUpload(event) {
+      const file = event.target.files[0];
+      this.imageFile = file;
+      this.toBase64(file).then((base64String) => {
+        this.imagePreview = base64String;
+      });
+    },
+    saveData() {
+      var token = sessionStorage.getItem("JWT");
+
+      const headers = {
+        'Authorization': "Bearer " + token,
+      }
+
+      this.table_data.blogUserId = this.blogUserId;
+
+      if (this.imageFile) {
+        this.toBase64(this.imageFile).then((base64String) => {
+          this.table_data.avatar = base64String;
+          axios.put("https://localhost:7185/api/Account/UserDetails", this.table_data, {
+            headers: headers,
+          })
+            .then((res) => {
+              console.log(res);
+              this.isEditing = false;
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+      } else {
+        axios.put("https://localhost:7185/api/Account/UserDetails", this.table_data, {
+          headers: headers,
+        })
+          .then((res) => {
+            console.log(res);
+            this.isEditing = false;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+    cancelEdit() {
+      this.isEditing = false;
+      this.imagePreview = this.table_data.avatar ? this.table_data.avatar : null;
+      this.table_data = {
+        ...this.table_data,
+        avatar: null // or default image url
+      };
+    },
+    async toBase64(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+      });
+    },
   }
 }
 </script>
@@ -91,5 +177,32 @@ export default {
   border: 1px solid #ccc;
 }
 
+.profile form label {
+  display: inline-block;
+  width: 100px;
+  text-align: left;
+  margin-bottom: 10px;
+}
+
+.profile form input {
+  display: inline-block;
+  width: calc(100% - 110px);
+  margin-bottom: 10px;
+}
+
+.username-label {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+.username-input {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+  font-size: 16px;
+  background-color: #f9f9f9;
+}
 </style>
 
