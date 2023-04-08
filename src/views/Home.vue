06e51b2b9e41ee8f90d8b82a4edff52be2cabcb2
GNUH <34486659+nvh202sua@users.noreply.github.com>
2023-04-08 17:24:36 +0700

    update home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,38 @@
 <template>
-    <div class="masonry-wrap">
-
-        <div class="masonry">
-
-            <div class="grid-sizer"></div>
-
-            <article class="masonry__brick entry format-standard " v-for="(item, index) in table_data" :key="index">
-
-                <div class="entry__thumb">
-                    <a href="single-standard.html" class="entry__thumb-link">
-                        <img src="public/images/thumbs/masonry/woodcraft-600.jpg"
-                            srcset="images/thumbs/masonry/woodcraft-600.jpg 1x, images/thumbs/masonry/woodcraft-1200.jpg 2x"
-                            alt="">
-                    </a>
-                </div>
-
-                <div class="entry__text">
-                    <div class="entry__header">
-
-                        <h2 class="entry__title"><a href="single-standard.html">Just a Standard Format Post.</a></h2>
-                        <div class="entry__meta">
-                            <span class="entry__meta-cat">
-                                <a href="category.html">Design</a>
-                                <a href="category.html">Photography</a>
-                            </span>
-                            <span class="entry__meta-date">
-                                <a href="single-standard.html">Apr 29, 2019</a>
-                            </span>
+    <div>
+        <div v-if="loading">Loading articles...</div>
+        <div v-else class="masonry-wrap">
+            <div class="masonry">
+                <div class="grid-sizer"></div>
+                <article class="masonry__brick entry format-standard " v-for="(item, index) in table_data" :key="index">
+                    <div class="entry__thumb">
+                        <a href="single-standard.html" class="entry__thumb-link">
+                            <img src="public/images/thumbs/masonry/woodcraft-600.jpg"
+                                srcset="images/thumbs/masonry/woodcraft-600.jpg 1x, images/thumbs/masonry/woodcraft-1200.jpg 2x"
+                                alt="">
+                        </a>
+                    </div>
+                    <div class="entry__text">
+                        <div class="entry__header">
+                            <h2 class="entry__title"><a href="single-standard.html">{{ item.title }}</a></h2>
+                            <div class="entry__meta">
+                                <span class="entry__meta-cat">
+                                    <a href="category.html">Design</a>
+                                    <a href="category.html">Photography</a>
+                                </span>
+                                <span class="entry__meta-date">
+                                    <a href="single-standard.html">{{ item.createDate }}</a>
+                                </span>
+                            </div>
                         </div>
-
+                        <div class="entry__excerpt">
+                            <p>{{ item.shortDescription }}</p>
+                        </div>
                     </div>
-                    <div class="entry__excerpt">
-                        <p>
-                            {{ item.text }}
-                        </p>
-                    </div>
-                </div>
-
-            </article> <!-- end article -->
-        </div> <!-- end masonry -->
-
-    </div> <!-- end masonry-wrap -->
+                </article>
+            </div>
+        </div>
+    </div>
 
     <div class="row">
         <div class="column large-full">
@@ -71,12 +63,9 @@ export default {
     },
     data() {
         return {
-            filterData: {
-            },
             table_data: [],
         }
     },
-
     created() {
         this.testApi();
     },
@@ -97,14 +86,18 @@ export default {
 
     methods: {
         testApi() {
-            axios.get("https://localhost:7185/api/Article", {
-            })
+            axios.get("https://localhost:7185/api/Article")
                 .then((res) => {
                     console.log(res.data);
                     let resc = res.data;
                     this.table_data = resc.result;
+                    this.loading = false;
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.loading = false;
                 });
-        }
+        },
     },
 };
 </script>
